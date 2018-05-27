b7da89b8429e0219373a0f0c8058a3f86298103d
Alexey Iskovskikh <iskovskikh@gmail.com>
2018-05-27 21:04:41 +0300

    Работа над карточкой товара прогдолжается

--- scripts/drop_db.sh
+++ scripts/drop_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source '../common.env'
-docker exec -i redtraktor_postgres_1 psql -c "DROP DATABASE $DB_NAME"
+source "$HOME/.bashrc"
+docker exec -i redtraktor_postgres_1 psql -U $DB_USER -c "DROP DATABASE $DB_NAME"
--- scripts/restore_db.sh
+++ scripts/restore_db.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source '../common.env'
-cat $1 | docker exec -i redtraktor_postgres_1 psql -U postgres
+source "$HOME/.bashrc"
+docker exec -i redtraktor_postgres_1 psql -U $DB_USER -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
+
+cat $1 | docker exec -i redtraktor_postgres_1 psql -U $DB_USER
